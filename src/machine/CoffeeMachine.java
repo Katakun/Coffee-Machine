2f9c72177a2afc0b2fbd865a682e3b93cb672fdf
alexandr <k4t4kun@gmail.com>
2021-09-01 19:49:40 +0900

    stage 4/6 complete

--- src/machine/CoffeeMachine.java
+++ src/machine/CoffeeMachine.java
@@ -1,4 +1,5 @@
 package machine;
+
 import java.util.Scanner;
 
 public class CoffeeMachine {
@@ -10,42 +11,69 @@ public class CoffeeMachine {
         int disposableCups = 9;
         int money = 550;
 
-        while (true) {
-            System.out.println("The coffee machine has:");
-            System.out.printf("%d ml of water", water);
-            System.out.printf("%d ml of milk", milk);
-            System.out.printf("%d g of coffee beans", beans);
-            System.out.printf("%d disposable cups", disposableCups);
-            System.out.printf("$%d of money", money);
+        cofeeMachineStatus(water, milk, beans, disposableCups, money);
 
-            System.out.println("Write action (buy, fill, take):");
-            String action = scan.nextLine();
-
-            switch (action) {
-                case "bue": {
-                    System.out.println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino: ");
-                    // метод покупки кофе
-                    break;
+        System.out.println("\n\nWrite action (buy, fill, take):");
+        String action = scan.nextLine();
+        switch (action) {
+            case "buy": {
+                System.out.println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino: ");
+                int choice = scan.nextInt();
+                switch (choice) {
+                    case 1:
+                        // espresso 250 ml of water and 16 g of coffee beans. It costs $4.
+                        water -= 250;
+                        beans -= 16;
+                        money += 4;
+                        disposableCups--;
+                        break;
+                    case 2:
+                        // latte 350 ml of water, 75 ml of milk, and 20 g of coffee beans. It costs $7.
+                        water -= 350;
+                        milk -= 75;
+                        beans -= 20;
+                        money += 7;
+                        disposableCups--;
+                        break;
+                    case 3:
+                        // cappuccino 200 ml of water, 100 ml of milk, and 12 g of coffee beans. It costs $6.
+                        water -= 200;
+                        milk -= 100;
+                        beans -= 12;
+                        money += 6;
+                        disposableCups--;
+                        break;
                 }
-                case "fill": {
-                    System.out.println("Write how many ml of water you want to add:");
-                    water += scan.nextInt();
-                    System.out.println("Write how many ml of milk you want to add:");
-                    milk += scan.nextInt();
-                    System.out.println("Write how many grams of coffee beans you want to add:");
-                    beans += scan.nextInt();
-                    System.out.println("Write how many disposable cups of coffee you want to add:");
-                    disposableCups += scan.nextInt();
-                    break;
-                }
-                case "take": {
-                    System.out.printf("I gave you $%d", money);
-                    money = 0;
-                     break;
-                }
+                break;
+            }
+            case "fill": {
+                System.out.println("Write how many ml of water you want to add:");
+                water += scan.nextInt();
+                System.out.println("Write how many ml of milk you want to add:");
+                milk += scan.nextInt();
+                System.out.println("Write how many grams of coffee beans you want to add:");
+                beans += scan.nextInt();
+                System.out.println("Write how many disposable cups of coffee you want to add:");
+                disposableCups += scan.nextInt();
+                break;
+            }
+            case "take": {
+                System.out.printf("I gave you $%d\n", money);
+                money = 0;
+                break;
             }
         }
 
+        cofeeMachineStatus(water, milk, beans, disposableCups, money);
+    }
+
+    public static void cofeeMachineStatus(int water, int milk, int beans, int disposableCups, int money) {
+        System.out.println("\nThe coffee machine has:");
+        System.out.printf("%d ml of water", water);
+        System.out.printf("\n%d ml of milk", milk);
+        System.out.printf("\n%d g of coffee beans", beans);
+        System.out.printf("\n%d disposable cups", disposableCups);
+        System.out.printf("\n$%d of money", money);
     }
 
     public static void howManyIngredients(int cupsOfCoffee) {
